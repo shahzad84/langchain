211ff63f6096947e4a923f50f11bad747767124f
shahzad84 <shahzad20022002@gmail.com>
2024-01-18 15:47:44 +0530

    We can also load a tiktoken splitter directly, which ensure each split is smaller than chunk size.

--- text_splitters/split_by_token/split_by_tokens.py
+++ text_splitters/split_by_token/split_by_tokens.py
@@ -1,14 +1,26 @@
 # Language models have a token limit. You should not exceed the token limit. When you split your text into chunks it is therefore a good idea to count the number of tokens. There are many tokenizers. When you count tokens in your text you should use the same tokenizer as used in the language model.
 # Note that if we use CharacterTextSplitter.from_tiktoken_encoder, text is only split by CharacterTextSplitter and tiktoken tokenizer is used to merge splits.
 
-with open("../sample/data.txt") as f:
+# with open("../sample/data.txt") as f:
+#     state_of_the_union=f.read()
+
+# from langchain.text_splitter import CharacterTextSplitter
+
+# text_splitter=CharacterTextSplitter.from_tiktoken_encoder(
+#     chunk_size=100,chunk_overlap=0
+# )
+
+# texts=text_splitter.split_text(state_of_the_union)
+# print(texts[0])
+
+
+# We can also load a tiktoken splitter directly, which ensure each split is smaller than chunk size.
+
+with open("../../sample/data.txt") as f:
     state_of_the_union=f.read()
 
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import TokenTextSplitter
 
-text_splitter=CharacterTextSplitter.from_tiktoken_encoder(
-    chunk_size=100,chunk_overlap=0
-)
-
+text_splitter=TokenTextSplitter(chunk_size=10,chunk_overlap=0)
 texts=text_splitter.split_text(state_of_the_union)
 print(texts[0])
